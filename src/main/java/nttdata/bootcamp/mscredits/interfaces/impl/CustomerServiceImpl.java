7df778388b6776da5279ade047f8c85ff1485c17
TheoRevFdz <hrevillafernandez79@gmail.com>
2022-10-31 10:39:13 -0500

    Add swagger

--- src/main/java/nttdata/bootcamp/mscredits/interfaces/impl/CustomerServiceImpl.java
+++ src/main/java/nttdata/bootcamp/mscredits/interfaces/impl/CustomerServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import nttdata.bootcamp.mscredits.config.RestConfig;
 import nttdata.bootcamp.mscredits.dto.CustomerDTO;
@@ -13,16 +14,18 @@ import nttdata.bootcamp.mscredits.interfaces.ICustomerService;
 
 @Service
 public class CustomerServiceImpl implements ICustomerService {
-    
+
     @Autowired
     private RestConfig rest;
+
+    @Value("${hostname}")
+    private String hostname;
 
     @Override
     public Optional<CustomerDTO> findCustomerByNroDoc(String nroDoc) throws ParseException {
         Map<String, String> param = new HashMap<String, String>();
         param.put("nroDoc", nroDoc);
-        String uri = "http://localhost:8090/api/customers/byNroDoc/{nroDoc}";
-        // String uri = "http://localhost:8081/byNroDoc/{nroDoc}";
+        String uri = String.format("http://%s:8090/api/customers/byNroDoc/{nroDoc}", hostname);
         CustomerDTO dto = rest.getForObject(
                 uri,
                 CustomerDTO.class,
